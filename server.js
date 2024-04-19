2b2251e92ce6c725b44033f854be3ed11def3317
404-d00d <raddivant@gmail.com>
2024-04-19 01:54:44 -0400

    added login feature

--- server.js
+++ server.js
@@ -7,8 +7,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // route for default splash page
 app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password, studentID } = req.body;
+
+    // Check if the submitted credentials match the valid credentials
+    if (
+      username === "username" &&
+      password === "password" &&
+      studentID === "123456789"
+    ) {
+      // If credentials are valid, redirect to the index page
+      console.log('Account login authenticated');
+      res.redirect('/index');
+    } else {
+      // If credentials are invalid, redirect back to the login page or display an error message
+      console.log('Invalid login credentials');
+      res.redirect('/'); // Redirect back to the login page for simplicity
+    }
+});
+
+// route for rendering the index.ejs page
+app.get('/index', (req, res) => {
   res.render('index');
 });
 
